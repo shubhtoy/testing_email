08bcaec0caa8de45e980ef97ac1b8dda46d7bda0
Shubh Mittal <shubhmittal.sm@gmail.com>
2023-07-28 22:34:12 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -119,6 +119,20 @@ def validate_emails(emails, progress_bar):
 
 def main():
     # Add a title to the main window
+    st.markdown(
+    """
+    <style>
+    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+    .viewerBadge_text__1JaDK {
+        display: none;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+    )
+
+
 
     st.set_page_config(page_title="Email Validator", page_icon=":envelope:")
     st.title("Email Address Validator")
